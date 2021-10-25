191e17b24c4ff73781eb501e9f73c130278133d8
Joffrey Nolting <joffrey.nolting@gmail.com>
2021-10-25 19:48:07 +0200

    Create bookRenamer.py
    just a start. it just renames a specific file

---
+++ bookRenamer.py
@@ -0,0 +1,8 @@
+import os
+
+# Absolute path of a file
+old_name = r"C:\Scripts\pythontestfiles\oldfile.txt"
+new_name = r"C:\Scripts\pythontestfiles\newfile.txt"
+
+# Renaming the file
+os.rename(old_name, new_name)
