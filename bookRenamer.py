97f340430b8370e4038cc8d0e4628c55ae4a5050
Joffrey Nolting <joffrey.nolting@gmail.com>
2021-10-25 19:49:02 +0200

    added Glob library and a search function

--- bookRenamer.py
+++ bookRenamer.py
@@ -1,8 +1,14 @@
 import os
+import glob
 
 # Absolute path of a file
 old_name = r"C:\Scripts\pythontestfiles\oldfile.txt"
 new_name = r"C:\Scripts\pythontestfiles\newfile.txt"
 
 # Renaming the file
-os.rename(old_name, new_name)
+#os.rename(old_name, new_name)
+
+
+for name in glob.glob('C:\Scripts\pythontestfiles\*,*.txt'):
+    print(name)
+    os.rename(name, new_name)
